bbc71d6ef7ab633385d641b03969fa434bde075a
Brenda-Machado <araucaria2011@gmail.com>
2023-11-24 16:20:45 -0300

    Nodos mandando mensagens entre si

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ Brenda Silva Machado - 2023.2
 main.py contains the class Main, which is used to run the program.
 
 """
+
+import threading
+import node
+
+class Main:
+    def __init__(self, nodes, request_q):
+        self.nodes = nodes
+        self.request_q = request_q
+
+    
--- node.py
+++ node.py
@@ -6,16 +6,15 @@ node.py contains the class Node, which is used to create the nodes of the tree.
 
 """
 
-import socket
-
 class Node:
 
-    def __init__(self, id):
+    def __init__(self, id, request_q):
 
         self.id = id
         self.using = False
         self.asked = False
         self.holder = None
+        self.request_q = request_q
         
     def get_id(self):
         return self.id
@@ -37,3 +36,26 @@ class Node:
     
     def set_holder(self, holder):
         self.holder = holder
+    
+    def make_request(self):
+        if self.holder != self and (not self.request_q.empty()) and (not self.asked):
+            self.asked = True
+            self.request_q.put(self)
+
+            print("Node " + str(self.id) + " made a request.")
+
+    def assign_privilege(self):
+        if self.holder == self and (not self.using) and (not self.request_q.empty()):
+            self.holder = self.request_q.get()
+            self.asked = False
+            
+            if self.holder == self:
+                self.using = True
+
+                """
+                Enters the critical section.
+                """
+            else:
+                self.holder.assign_privilege()
+
+        print("Node " + str(self.holder.get_id()) + " get the token.")
